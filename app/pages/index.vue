8f32dd767242768e3fce1c9bb86d2526692a9603
Xiao-xiaocn <xiao-xiaocn@outlook.com>
2025-10-12 16:22:57 +0800

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,11 @@
 <template>
   <main class="flex flex-col justify-center">
+    <section class="my-custom-section">
+      <h2>我的自定义标题</h2>
+      <p>这是我的自定义段落内容</p>
+      <div class="custom-container">
+        <button class="custom-button">点击我</button>
+      </div>
+    </section>
   </main>
 </template>
