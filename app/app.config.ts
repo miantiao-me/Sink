a506c1c84600d661cbad8c3b4d8bda479374287e
onyst-it <102585459+onyst-it@users.noreply.github.com>
2026-03-08 03:04:24 -0700

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,14 +1,12 @@
 export default defineAppConfig({
-  title: 'Sink',
-  github: 'https://github.com/miantiao-me/sink',
-  coffee: 'https://sink.cool/coffee',
-  twitter: 'https://sink.cool/x',
-  telegram: 'https://sink.cool/telegram',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
-  previewTTL: 300, // 5 minutes
+  title: 'ONYST Link Shortener',
+  github: '',
+  coffee: '',
+  twitter: '',
+  telegram: '',
+  description: '',
+  image: '',
+  previewTTL: 300,
   slugRegex: /^[a-zA-Z0-9_-]+$/,
-  reserveSlug: [
-    'dashboard',
-  ],
+  reserveSlug: ['dashboard'],
 })
