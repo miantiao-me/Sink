67a840d62a82d3c09bf2d358c1f6830a7c4dfacf
onyst-it <102585459+onyst-it@users.noreply.github.com>
2026-03-07 22:54:34 -0800

    Delete functions directory

--- functions/_middleware.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const hostname = context.request.headers.get('host') || ''
-  
-  if (hostname.endsWith('pages.dev')) {
-    return new Response('Access Denied', { status: 403 })
-  }
-  
-  return await context.next()
-}
